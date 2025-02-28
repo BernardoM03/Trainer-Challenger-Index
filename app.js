76e8f8a161d19cfc338415af063fad802fbb3100
Gage Davelaar <123704043+gdav1138@users.noreply.github.com>
2025-02-27 19:53:28 -0800

    Update app.js
    Updated app.js with app.get for each hbs page as well as update/delete query functions for pokemon and delete query function for gyms

--- app.js
+++ app.js
@@ -56,6 +56,63 @@ app.get('/delete_trainer/:id', function(req, res) {
     })
 });
 
+app.get('/pokemon', function(req, res)
+{
+    let query1 = `SELECT Pokemon.name AS pokemon_name, Pokemon.type, Trainers.name AS trainer_name, Regions.name AS region_name
+                  FROM Pokemon
+                  INNER JOIN Trainers ON Pokemon.trainer_id = Trainers.trainer_id
+                  INNER JOIN Regions ON Pokemon.region_id = Regions.region_id;`;
+    db.pool.query(query1, function(error, rows, fields){
+        res.render('pokemon', {data: rows});
+    })
+});
+
+app.get('/delete_pokemon/:id', function(req, res) {
+    const pokemonId = req.params.id;
+    let query = "DELETE FROM Pokemon WHERE pokemon_id = ?";
+    db.pool.query(query, [pokemonId], function(error, results) {
+        res.redirect('/pokemon');
+    })
+});
+
+app.get('/gyms', function(req, res)
+{
+    let query1 = `SELECT Gyms.name AS gym_name, Gyms.type, Regions.name AS region_name
+                  FROM Gyms
+                  INNER JOIN Regions ON Gyms.region_id = Regions.region_id;`;
+    db.pool.query(query1, function(error, rows, fields){
+        res.render('gyms', {data: rows});
+    })
+});
+
+app.get('/delete_gyms/:id', function(req, res) {
+    const gymId = req.params.id;
+    let query = "DELETE FROM Gyms WHERE gym_id = ?";
+    db.pool.query(query, [gymId], function(error, results) {
+        res.redirect('/gyms');
+    })
+});
+
+app.get('/regions', function(req, res)
+{
+    let query1 = `SELECT name
+                  FROM Regions;`;
+    db.pool.query(query1, function(error, rows, fields){
+        res.render('regions', {data: rows});
+    })
+});
+
+app.get('/trainerGyms', function(req, res)
+{
+    let query1 = `SELECT Trainers.name AS trainer_name, TrainerGyms.badge_name, TrainerGyms.date_earned
+                  FROM TrainerGyms
+                  INNER JOIN Trainers ON TrainerGyms.trainer_id = Trainers.trainer_id;`;
+    db.pool.query(query1, function(error, rows, fields){
+        res.render('trainerGyms', {data: rows});
+    })
+});
+
+
 /*
     LISTENER
 */
